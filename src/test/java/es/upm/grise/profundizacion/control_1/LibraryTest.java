8d2e236fc384374d481e09cdf37a1458651239ce
Sandra Sierra <sandra.sierra@vipera.com>
2022-11-07 16:58:30 +0100

    Add tests

--- src/test/java/es/upm/grise/profundizacion/control_1/LibraryTest.java
+++ src/test/java/es/upm/grise/profundizacion/control_1/LibraryTest.java
@@ -1,10 +1,22 @@
 package es.upm.grise.profundizacion.control_1;
 
-public class LibraryTest {
-	
-	@Test
-	public void test() {
-		fail();
-	}
+import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class LibraryTest {
+
+	private final String title1 = "title_1";
+	private final Book book1 = new Book(title1);
+
+	@Test
+	public void addBookTest() throws DuplicatedBookException, NonExistingBookException, EmptyLibraryException {
+		Library library = mock(Library.class);
+		when(library.getBook(title1)).thenReturn(book1);
+
+		library.addBook(book1);
+		assertEquals(book1, library.getBook(title1));
+	}
 }
